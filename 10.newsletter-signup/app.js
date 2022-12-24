30f75a04a095ccbd20cacd01e80020e4b12ca521
Muhammad Ali <aliadison@gmail.com>
2022-12-24 18:46:31 -0500

    newsletter signup mailchimp api

--- 10.newsletter-signup/app.js
+++ 10.newsletter-signup/app.js
@@ -1,22 +1,45 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const mailchimp = require("@mailchimp/mailchimp_marketing");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", function (req, res){
-    res.sendFile(__dirname + "/signup.html");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/", function(req, res){
-    var firstName = req.body.fName;
-    var lastName = req.body.lName;
-    var email = req.body.email;
+mailchimp.setConfig({
+  apiKey: process.env.API_KEY,
+  server: "us10",
+});
 
-    console.log(firstName, lastName, email);
+app.post("/", function (req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const run = async () => {
+    try {
+      const response = await mailchimp.lists.batchListMembers(process.env.LIST_ID, {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      });
+      console.log(response);
+      res.sendFile(__dirname + "/success.html");
+    } catch (err) {
+      console.log(err.status);
+      res.sendFile(__dirname + "/failure.html");
+    }
+  };
+  run();
 });
 
 app.listen(3000, function () {
